e51e6287d9868a195620e6326a91a63ea602d0ae
vt3 <vt3@DIN86001000.corp.capgemini.com>
2020-07-26 14:45:07 +0530

    Second Controller pushed

---
+++ SpringBootSample/src/main/java/com/example/SpringBootSample/Controller/SecondController.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSample.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecondController {
+
+	
+	public void getData() {
+		
+		System.out.println("THis is second controller");
+	}
+}
